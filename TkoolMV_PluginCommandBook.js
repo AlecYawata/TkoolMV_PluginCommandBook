4fba9e622d930d7ad83ee82c5c227e335930ff91
AlecYawata <alec.yawata@gmail.com>
2015-12-09 02:22:18 +0900

    プラグインコマンド集用のプラグインファイルを作成

---
+++ TkoolMV_PluginCommandBook.js
@@ -0,0 +1,29 @@
+//=============================================================================
+// TkoolMV_PluginCommandBook.js
+// https://github.com/AlecYawata/TkoolMV_PluginCommandBook
+//=============================================================================
+//
+// Copyright (c) 2015 Alec
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// 
+// * @plugindesc プラグインコマンド集
+// * @author 有志の皆さん
+// 
+//=============================================================================
+/*:
+ * @help
+ */
+
+
+(function(){
+
+	/**
+	 * @param Screen Height
+ 	 * @desc Adjusts the height of the screen.
+ 	 * Default: 624
+ 	 * @default 624
+
+	 */
+
+})();
