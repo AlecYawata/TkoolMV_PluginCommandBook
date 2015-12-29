05eb1b508bd3daae987d1ea993799d14cf5a08c4
トリアコンタン <triacontanetakenoko@gmail.com>
2015-12-29 13:19:14 +0900

    コマンド追加、'use strict'の適用、共通関数「parseIntStrict」追加
    １．数値入力の処理関連のコマンド3つ、ピクチャ関連のコマンド4つを追加しました。
    
    ２．複数人が編集することを考慮し、'use strict'を適用させました。グローバル汚染を未然に防ぐ等の効果があります。
    
    ３．引数の数値チェックをして数値でない場合は例外を発生させる関数「parseIntStrict」を追加しました。
    プラグインコマンド集では例外が発生してもその場でゲームを中断せず、コンソールにログが出力されます。
    ユーザに引数の指定が間違っていることを明確に伝えることができまでのでよろしければお使いください。

--- SampleProject/js/plugins.js
+++ SampleProject/js/plugins.js
@@ -4,5 +4,7 @@ var $plugins =
 [
 {"name":"TkoolMV_PluginCommandBook","status":true,"description":"プラグインコマンド集","parameters":{"制御文字の拡張":"はい"}},
 {"name":"CustomizeConfigDefault","status":true,"description":"Optionsデフォルト値設定プラグイン","parameters":{"alwaysDash":"ON","commandRemember":"OFF","bgmVolume":"100","bgsVolume":"100","meVolume":"100","seVolume":"100"}},
-{"name":"DTextPicture","status":true,"description":"動的文字列ピクチャ生成プラグイン","parameters":{}}
+{"name":"DTextPicture","status":true,"description":"動的文字列ピクチャ生成プラグイン","parameters":{}},
+{"name":"DevToolsManage","status":true,"description":"デベロッパツール管理プラグイン","parameters":{"開始時に起動":"ON","常に前面表示":"ON","デベロッパツール表示位置":"0,0,1200,800","最小化切替キー":"F8","リロードキー":"F12","画面の左寄せキー":"F11"}},
+{"name":"MapRapid","status":true,"description":"マップ高速化プラグイン","parameters":{"showMessageRapid":"ON","windowOpenRapid":"ON"}}
 ];
